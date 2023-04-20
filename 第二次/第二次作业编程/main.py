5b00c242630411bd17bd61311238648e761b5ad2
Myking-6 <71422217+Myking-6@users.noreply.github.com>
2023-04-20 09:20:41 +0800

    Add files via upload

--- 第二次/第二次作业编程/main.py
+++ 第二次/第二次作业编程/main.py
@@ -7,6 +7,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import line_search
 
 n = 4
 m = n
@@ -34,41 +35,12 @@ def df(x):
     return 2 * np.dot(J.T, r(x))
 
 
-def line_search_wolfe(fun, x0, g, alpha=1, c1=1e-4, c2=0.9):
-    phi0 = fun(x0)
-    dphi0 = np.dot(df(x0), alpha)
-    phi = lambda a: fun(x0 + np.dot(a, alpha))
-    dphi = lambda a: np.dot(df(x0 + np.dot(a, alpha)), alpha)
-    a = 0
-    b = np.inf
-    while True:
-        if phi(alpha).all() > (phi0 + c1 * alpha * dphi0).all() or (phi(alpha).all() >= phi(a).all() and a != 0):
-            b = alpha
-            alpha = (a + b) / 2
-        else:
-            if abs(dphi(alpha)).all() <= (-c2 * dphi0).all():
-                break
-            if dphi(alpha).all() >= 0:
-                b = alpha
-                alpha = (a + b) / 2
-            else:
-                a = alpha
-                if b == np.inf:
-                    alpha = 2 * a
-                else:
-                    alpha = (a + b) / 2
-    return alpha
-
-
 class SR1:
     def __init__(self, n):
         self.Hk = np.eye(n)
 
     def update(self, sk, yk):
-        yksk = yk - np.dot(self.Hk, sk)
-        yksk_norm = np.dot(yksk, sk)
-        if yksk_norm > 0:
-            self.Hk += np.outer(yksk, yksk) / yksk_norm
+        self.Hk = self.Hk + np.linalg.norm(sk - np.dot(self.Hk, yk)) / np.dot((sk - np.dot(self.Hk, yk)).T, yk)
 
     def dot(self, gk):
         return np.dot(self.Hk, gk)
@@ -79,9 +51,8 @@ class DFP:
         self.Hk = np.eye(n)
 
     def update(self, sk, yk):
-        if np.dot(yk, sk) > 0:
-            self.Hk = self.Hk - np.outer(np.dot(self.Hk, sk), np.dot(self.Hk, sk)) / np.dot(np.dot(sk, self.Hk), sk) \
-                      + np.outer(yk, yk) / np.dot(yk, sk)
+        self.Hk = self.Hk - np.linalg.norm(np.dot(self.Hk, yk)) / np.dot(np.dot(yk.T, self.Hk), yk) + np.linalg.norm(
+            sk) / np.dot(yk.T, sk)
 
     def dot(self, gk):
         return np.dot(self.Hk, gk)
@@ -92,11 +63,9 @@ class BFGS:
         self.Hk = np.eye(n)
 
     def update(self, sk, yk):
-        yksk = yk - np.dot(self.Hk, sk)
-        yksk_norm = np.dot(yksk, sk)
-        if yksk_norm > 0:
-            self.Hk += np.outer(yksk, yksk) / yksk_norm \
-                       - (np.outer(np.dot(self.Hk, sk), np.dot(self.Hk, sk)) / np.dot(sk, np.dot(self.Hk, sk)))
+        pk = 1 / np.dot(sk.T, yk)
+        self.Hk = np.dot(np.dot((np.eye(n) - np.dot(np.dot(pk, yk), sk.T)).T, self.Hk),
+                         np.eye(n) - np.dot(np.dot(pk, yk), sk.T)) + np.dot(np.dot(pk, sk), sk.T)
 
     def dot(self, gk):
         return np.dot(self.Hk, gk)
@@ -104,41 +73,38 @@ class BFGS:
 
 def optimize_with_method(method):
     x = x0
-    alpha = 1
     results = [(x, f(x))]
-    #for i in range(100):
+    alpha = 1
     while True:
         g = df(x)
-        p = -method.dot(g)
-        alpha = line_search_wolfe(f, x, g, alpha=alpha)
-        s = alpha * p
+        d = -method.dot(g)
+        alpha_k = line_search(f=f, myfprime=df, xk=x, pk=d, c2=0.9)[0]
+        if alpha_k == None:
+            break
+        elif isinstance(alpha_k, float):
+            alpha = alpha_k
+        else:
+            alpha = alpha_k.squeeze()
+        print(alpha, end="")
+        s = alpha * d
         y = df(x + s) - g
         method.update(s, y)
         x = x + s
-        results.append((x, f(x)))
-        if np.linalg.norm(s) < 1e-8:
+        f_value = f(x)
+        print(x, f_value)
+        results.append((x, f_value))
+        if np.linalg.norm(f_value) < 1e-8:
             break
     return results
 
 
-results_sr1 = optimize_with_method(SR1(n))
-results_dfp = optimize_with_method(DFP(n))
-results_bfgs = optimize_with_method(BFGS(n))
-print("SR1:"+str(len(results_sr1))+",DFP:"+str(len(results_dfp))+",BFGS:"+str(len(results_bfgs)))
+methods = [SR1, DFP, BFGS]
 
-plt.figure(1)
-plt.plot([r[1] for r in results_sr1], label='SR1')
-plt.legend()
-plt.xlabel('Iteration')
-plt.ylabel('Objective value')
-plt.figure(2)
-plt.plot([r[1] for r in results_dfp], label='DFP')
-plt.legend()
-plt.xlabel('Iteration')
-plt.ylabel('Objective value')
-plt.figure(3)
-plt.plot([r[1] for r in results_bfgs], label='BFGS')
-plt.legend()
-plt.xlabel('Iteration')
-plt.ylabel('Objective value')
-plt.show()
+for method in methods:
+    results_m = optimize_with_method(method(n))
+    print(method.__name__, ':', len(results_m))
+    plt.plot([r[1] for r in results_m], label=method.__name__)
+    plt.legend()
+    plt.xlabel('Iteration')
+    plt.ylabel('Objective value')
+    plt.show()
